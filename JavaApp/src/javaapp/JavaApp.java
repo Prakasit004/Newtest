a475e5211714ca37d6ed857ebc74d3debe91e0c4
Thanyalak Sirikul <s5730213014@phuket.psu.ac.th>
2018-04-09 18:29:35 +0700

    Merge origin/master into HEAD

--- JavaApp/src/javaapp/JavaApp.java
+++ JavaApp/src/javaapp/JavaApp.java
@@ -17,6 +17,7 @@ public class JavaApp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Thanyalak Sirikul");
+            System.out.println("Thanyalak Sirikul12345");
     }
     
 }
